86197e9ca29c27fb9eca49453fae639b420ec473
NirBenita <nirbenita@Benitas-MacBook.local>
2018-11-30 19:28:28 +0200

    change animation to non blocking

--- oracle_arduino/cube_animation.h
+++ oracle_arduino/cube_animation.h
@@ -25,7 +25,7 @@ CRGB leds[NUM_LEDS];
 #define BRIGHTNESS          96
 #define FRAMES_PER_SECOND  120
 
-void setup() {
+void setup_cubes() {
   delay(3000); // 3 second delay for recovery
   
   // tell FastLED about the LED strip configuration
@@ -43,20 +43,20 @@ SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, 
 
 uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
-  
-void loop()
-{
-  // Call the current pattern function once, updating the 'leds' array
-  gPatterns[gCurrentPatternNumber]();
 
-  // send the 'leds' array out to the actual LED strip
-  FastLED.show();  
-  // insert a delay to keep the framerate modest
-  FastLED.delay(1000/FRAMES_PER_SECOND); 
+uint8_t is_cube_active = 0;  
+void loop_cubes()
+{
+  if (is_cube_active == 0) {
+      returnl; 
+      
+  }
+  gCurrentPatternNumber = 0;
+  EVERY_N_MILLISECONDS(1000/FRAMES_PER_SECOND) {gPatterns[gCurrentPatternNumber]();  FastLED.show(); }
 
   // do some periodic updates
   EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
-  EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+  //EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
 }
 
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
